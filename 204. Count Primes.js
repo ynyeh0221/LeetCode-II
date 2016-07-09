745abaaa205097ebe13112a13b4d0ab4331e46b0
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-09 14:55:46 -0400

    Update 204. Count Primes.js

--- 204. Count Primes.js
+++ 204. Count Primes.js
@@ -7,21 +7,17 @@ var countPrimes = function(n) {
     var T = [];
     for (var i = 0; i < n; i++)
     {
-        if (i === 0 || i === 1)
-            T.push(0);
-        else
-            T.push(1);
+        if (i === 0 || i === 1) T.push(0);
+        else T.push(1);
     }
     for (i = 2; i*i <= n; i++)
     {
         for (var j = i; j < n; j += i)
         {
-            if (T[j] == 1 && i != j)
-                T[j] = 0;
+            if (T[j] == 1 && i != j) T[j] = 0;
         }
     }
     var res = 0;
-    for (i = 0; i < n; i++)
-        res += T[i];
+    for (i = 0; i < n; i++) res += T[i];
     return res;
 };
