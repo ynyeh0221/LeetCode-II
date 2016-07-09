995ad6e125868f36db70298c59056e78f4399897
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-09 14:55:05 -0400

    Create 204. Count Primes.cpp

---
+++ 204. Count Primes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n == 0 || n == 1)
+            return 0;
+        vector <int> T (n, 1);
+        T[0] = 0;
+        T[1] = 0;
+        for (int i = 2; i*i <= n; i++)
+        {
+            for (int j = i; j < n; j += i)
+            {
+                if (T[j] == 1 && j != i)
+                    T[j] = 0;
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < n; i++)
+            res += T[i];
+        return res;
+    }
+};
