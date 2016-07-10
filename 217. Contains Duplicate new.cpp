ec9483559a9012e576397764b2d513cb9ef92ef3
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-10 19:27:09 -0400

    Create 217. Contains Duplicate new.cpp

---
+++ 217. Contains Duplicate new.cpp
@@ -0,0 +1,8 @@
+#include <unordered_map>
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set <int> s(nums.begin(), nums.end());
+        return s.size() == nums.size() ? false : true;
+    }
+};
