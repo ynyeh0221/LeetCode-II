24aa8d5eacc7484a64b8a18c3ede45381a7ee79d
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-09 22:52:46 -0400

    Update 274. H-Index.py

--- 274. H-Index.py
+++ 274. H-Index.py
@@ -5,9 +5,8 @@ class Solution(object):
         :rtype: int
         """
         n = len(citations)
-        if n == 0 or all(x == 0 for x in citations):
-            return 0
         citations = sorted(citations)
         for i in xrange(n):
             if n-i <= citations[i]:
                 return n-i
+        return 0
