7bf795ce4c7879bb8a3b46d5779a4053c9447859
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-09 23:19:53 -0400

    Create 274. H-Index new.cpp

---
+++ 274. H-Index new.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        sort(citations.begin(), citations.end());
+        long long int l = 0, r = n;
+        while (l < r)
+        {
+            long long int mid = (l+r) >> 1;
+            if (citations[mid] >= n-mid)
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return n-l;
+    }
+};
