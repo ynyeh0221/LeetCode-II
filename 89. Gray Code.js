f11fa589ebb85a6a865dbb373e231abaf7c629b5
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-11 20:15:26 -0400

    Update 89. Gray Code.js

--- 89. Gray Code.js
+++ 89. Gray Code.js
@@ -6,10 +6,8 @@ var grayCode = function(n) {
     let res = [0];
     for (let i = 0; i < n; i++)
     {
-        let temp = [];
-        for (let j = 0; j < res.length; j++)
-            temp.push(res[j] + Math.pow(2, i));
-        res = res.concat(temp.reverse());
+        for (let j = res.length-1; j >= 0; j--)
+            res.push(res[j] + Math.pow(2, i));
     }
     return res;
 };
