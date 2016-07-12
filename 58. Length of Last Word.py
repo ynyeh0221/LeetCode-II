b456e15f7204176d45ae9c94075793db588da781
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-11 23:05:30 -0400

    Update 58. Length of Last Word.py

--- 58. Length of Last Word.py
+++ 58. Length of Last Word.py
@@ -5,10 +5,9 @@ class Solution(object):
         :rtype: int
         """
         s = s.rstrip()
-        if not s:
-            return 0
         for i in xrange(len(s)-1, -1, -1):
             if s[i] == ' ':
                 return len(s) - i - 1
             if i == 0:
                 return len(s) - i
+        return 0
