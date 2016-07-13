f37889f72d298737f0e3e872ce8ccf1d2eb54db4
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-13 16:42:28 -0400

    Update 205. Isomorphic Strings.py

--- 205. Isomorphic Strings.py
+++ 205. Isomorphic Strings.py
@@ -11,8 +11,6 @@ class Solution(object):
             if s[i] not in s_to_t and t[i] not in t_to_s:
                 s_to_t[s[i]] = t[i]
                 t_to_s[t[i]] = s[i]
-            elif s[i] in s_to_t and t[i] in t_to_s and s_to_t[s[i]] == t[i] and t_to_s[t[i]] == s[i]:
-                continue
-            else:
+            elif not (s[i] in s_to_t and t[i] in t_to_s and s_to_t[s[i]] == t[i] and t_to_s[t[i]] == s[i]):
                 return False
         return True
