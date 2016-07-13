30b2802161ac59465c2684c85212b77dba7ff8a1
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-13 16:44:13 -0400

    Update 205. Isomorphic Strings.py

--- 205. Isomorphic Strings.py
+++ 205. Isomorphic Strings.py
@@ -5,12 +5,10 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        s_to_t = {}
-        t_to_s = {}
+        s_to_t, t_to_s = {}, {}
         for i in xrange(len(s)):
             if s[i] not in s_to_t and t[i] not in t_to_s:
-                s_to_t[s[i]] = t[i]
-                t_to_s[t[i]] = s[i]
+                s_to_t[s[i]], t_to_s[t[i]] = t[i], s[i]
             elif not (s[i] in s_to_t and t[i] in t_to_s and s_to_t[s[i]] == t[i] and t_to_s[t[i]] == s[i]):
                 return False
         return True
