600f16061d514d66e423f30b2269423253a1ad49
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-13 14:16:39 -0400

    Create 136. Single Number.js

---
+++ 136. Single Number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let res = nums[0];
+    for (let i = 1; i < nums.length; i++)
+        res ^= nums[i];
+    return res;
+};
