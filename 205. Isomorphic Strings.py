329c870f1fb4e9789b31d06c503043ac56c504ea
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-13 16:39:02 -0400

    Create 205. Isomorphic Strings.py

---
+++ 205. Isomorphic Strings.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s_to_t = {}
+        t_to_s = {}
+        for i in xrange(len(s)):
+            if s[i] not in s_to_t and t[i] not in t_to_s:
+                s_to_t[s[i]] = t[i]
+                t_to_s[t[i]] = s[i]
+            elif s[i] in s_to_t and t[i] in t_to_s and s_to_t[s[i]] == t[i] and t_to_s[t[i]] == s[i]:
+                continue
+            else:
+                return False
+        return True
