5519188c4f8e251a235627a6e7895dda35308a98
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-13 18:04:23 -0400

    Create 338. Counting Bits.py

---
+++ 338. Counting Bits.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        res = [0,1,1,2,1,2,2,3]
+        ind = 4
+        while len(res) <= num:
+            temp = res[ind:]
+            ind = len(res)
+            res += temp + [x+1 for x in temp]
+        return res[:num + 1]
