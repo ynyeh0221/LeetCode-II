bfd746cd5b5cfb1f9276627a4c3e1ab8d56dc507
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-13 23:25:25 -0400

    Create 102. Binary Tree Level Order Traversal.py

---
+++ 102. Binary Tree Level Order Traversal.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        queue = [(root, 0)]
+        res = [[]]
+        while queue:
+            (root, level) = queue.pop(0)
+            res[len(res)-1] += [root.val]
+            if root.left:
+                queue += [(root.left, level+1)]
+            if root.right:
+                queue += [(root.right, level+1)]
+            if queue and level != queue[0][1]:
+                res += [[]]
+        return res
