faf0a8a043b492a77aa502ce33ffae839f8adb42
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-17 17:27:25 -0400

    Create 315. Count of Smaller Numbers After Self.py

---
+++ 315. Count of Smaller Numbers After Self.py
@@ -0,0 +1,39 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+        self.count = 1
+
+class Solution(object):
+    def countSmaller(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        if not nums:
+            return []
+        res = [0]
+        root = TreeNode(nums[len(nums) - 1])
+        for i in xrange(len(nums)-2, -1, -1):
+            res += [self.insert(root, nums[i])];
+        return res[::-1]
+    
+    def insert(self, root, t):
+        count = 0
+        while True:
+            if t <= root.val:
+                root.count += 1
+                if not root.left:
+                    root.left = TreeNode(t)           
+                    break
+                else:
+                    root = root.left
+            else:
+                count += root.count
+                if not root.right:
+                    root.right = TreeNode(t)           
+                    break
+                else:
+                    root = root.right
+        return count
