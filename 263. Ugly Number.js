eb1be0ff615bf3e3307521301a75265b4b591262
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-21 17:01:54 -0400

    Create 263. Ugly Number.js

---
+++ 263. Ugly Number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isUgly = function(num) {
+    if (num <= 0)
+        return false;
+    while (num % 5 === 0)
+        num /= 5;
+    while (num % 3 === 0)
+        num /= 3;
+    while (num % 2 === 0)
+        num /= 2;
+    return num == 1 ? true : false;
+};
