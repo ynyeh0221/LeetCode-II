adfd15846a073efb814f0dc6d21ea01469a9af22
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-21 17:02:31 -0400

    Update 263. Ugly Number.py

--- 263. Ugly Number.py
+++ 263. Ugly Number.py
@@ -6,10 +6,10 @@ class Solution(object):
         """
         if num <= 0:
             return False
-        while num % 2 == 0:
-            num /= 2
-        while num % 3 == 0:
-            num /= 3
         while num % 5 == 0:
             num /= 5
+        while num % 3 == 0:
+            num /= 3
+        while num % 2 == 0:
+            num /= 2
         return True if num == 1 else False
