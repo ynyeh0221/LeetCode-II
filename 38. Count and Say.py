1ae081261ed8cd42690bbab494a98ea02042e7c7
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-21 00:25:19 -0400

    Create 38. Count and Say.py

---
+++ 38. Count and Say.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        res = "1"
+        for i in xrange(2, n+1):
+            count = 1
+            temp = ""
+            for j in xrange(1, len(res)):
+                if res[j] != res[j-1]:
+                    temp += str(count) + res[j-1]
+                    count = 1
+                else:
+                    count += 1
+            temp += str(count) + res[len(res)-1]
+            res = temp
+        return res
