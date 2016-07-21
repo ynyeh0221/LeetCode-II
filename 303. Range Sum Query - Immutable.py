70b0295087bcd77a2633ef9763fe0d31e74c6135
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-20 23:54:04 -0400

    Create 303. Range Sum Query - Immutable.py

---
+++ 303. Range Sum Query - Immutable.py
@@ -0,0 +1,26 @@
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """
+        if not nums:
+            return
+        self.totalsum = nums
+        for i in xrange(1, len(self.totalsum)):
+            self.totalsum[i] += self.totalsum[i - 1]
+
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return self.totalsum[j] - self.totalsum[i-1] if i > 0 else self.totalsum[j]
+
+
+# Your NumArray object will be instantiated and called as such:
+# numArray = NumArray(nums)
+# numArray.sumRange(0, 1)
+# numArray.sumRange(1, 2)
